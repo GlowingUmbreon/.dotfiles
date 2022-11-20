f7ecd48bcbbf7b91d79cf4f48e9335a90dd6701f
GlowingUmbreon <umbreon@disroot.org>
2022-11-20 19:53:38 +0000

    Updated configs

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 # programs
 ln -s ~/.dotfiles/programs/alacritty ~/.config
-ln -s ~/.dotfiles/programs/bspwm ~/.config
 ln -s ~/.dotfiles/programs/chrome ~/.mozilla/firefox/urc4nnbz.default-default
 ln -s ~/.dotfiles/programs/mpv ~/.config
+ln -s ~/.dotfiles/programs/nvim ~/.config
 ln -s ~/.dotfiles/programs/picom ~/.config
-ln -s ~/.dotfiles/programs/sxhkd ~/.config
+ln -s ~/Sync/newsboat ~/.config
 
+# $HOME configs
 ln -s ~/.dotfiles/.zshrc ~
+ln -s ~/.dotfiles/.profile ~
 
-#ln -s ~/Sync/freetube/playlists.db ~/.config/FreeTube
-#ln -s ~/Sync/freetube/subscriptions.db ~/.config/FreeTube
-#ln -s ~/Sync/newsboat ~/.config
+# Freetube
+ln -s ~/Sync/freetube/playlists.db ~/.config/FreeTube
+ln -s ~/Sync/freetube/subscriptions.db ~/.config/FreeTube
